6ac1d17e8d8d9640d7ee78d1c83577fc448ec570
Thomas Trotter <140984323+Climber1705@users.noreply.github.com>
2025-03-28 00:18:04 +0000

    Update Randomizer.java

--- src/main/java/com/tomtrotter/habitatsimulation/model/Randomizer.java
+++ src/main/java/com/tomtrotter/habitatsimulation/model/Randomizer.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 /**
 * Provide control over the randomization of the simulation. By using the
-* shared, fixed-seed randomizer, repeated runs will perform exactly the same
-* (which helps with testing). Set 'useShared' too false to get different random
+* shared, fixed-seed randomizer, repeated runs will perform the same
+* (which helps with testing). Set 'useShared' to false to get different random
 * behaviour every time.
 */
 
