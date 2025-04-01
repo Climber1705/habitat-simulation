38aca084e7be535de597b7b96535f6e66adb7f49
Thomas Trotter <140984323+Climber1705@users.noreply.github.com>
2025-04-01 17:37:18 +0100

    Add files via upload

---
+++ src/main/java/com/tomtrotter/habitatsimulation/model/Prey.java
@@ -0,0 +1,34 @@
+package com.tomtrotter.habitatsimulation.model;
+
+import java.util.List;
+
+/**
+* A class representing shared behaviours of prey.
+* This interface defines the shared behaviors for prey animals, including grazing on plants.
+*/
+
+public interface Prey {
+
+    /**
+    * Logic for Prey finding and consuming plants.
+    * The prey will search for plants in adjacent locations and graze on the first plant found.
+    *
+    * @param prey: The animal grazing (the prey animal looking for food).
+    * @return The location where the plant was found and consumed, or null if no plant was found.
+    */
+    default Location graze(Animal prey) {
+        Field field = prey.getField();
+        List<Location> adjacent = field.adjacentLocations(prey.getLocation());
+
+        for (Location where: adjacent) {
+            Object object = field.getObjectAt(where);
+            if (object instanceof Plant plant) {
+                plant.setDead();
+                prey.setFoodLevel(Animal.PLANT_FOOD_VALUE);
+                return where;
+            }
+        }
+        return null;
+    }
+
+}
